a1a7fa604a2ab992791cc3c40c56dec0483d5125
Shridhar-dev <shridharkamat10@gmail.com>
2020-12-03 14:34:42 +0530

    first one

--- src/Chat.js
+++ src/Chat.js
@@ -21,7 +21,6 @@ function Chat() {
     return (
         <div class="container py-0 m-0 " style={{height:'100vh',width:'100%',maxWidth:'100%'}}>
         <div class="d-flex  justify-content-between align-items-center  " style={{width:'100%',height:'10%',overflowX:'auto'}}>
-            <hr style={{position:'absolute',color:'black',height:'2px',width:'67%',left:'33%',top:'7.3%'}}/>
         <div class="d-flex align-items-center">
         <svg width="2em" height="2em" viewBox="0 0 24 24" class=" mx-3  icon-1DeIlz"><path fill="rgba(185,187,190,0.5)" fill-rule="evenodd" clip-rule="evenodd" d="M5.88657 21C5.57547 21 5.3399 20.7189 5.39427 20.4126L6.00001 17H2.59511C2.28449 17 2.04905 16.7198 2.10259 16.4138L2.27759 15.4138C2.31946 15.1746 2.52722 15 2.77011 15H6.35001L7.41001 9H4.00511C3.69449 9 3.45905 8.71977 3.51259 8.41381L3.68759 7.41381C3.72946 7.17456 3.93722 7 4.18011 7H7.76001L8.39677 3.41262C8.43914 3.17391 8.64664 3 8.88907 3H9.87344C10.1845 3 10.4201 3.28107 10.3657 3.58738L9.76001 7H15.76L16.3968 3.41262C16.4391 3.17391 16.6466 3 16.8891 3H17.8734C18.1845 3 18.4201 3.28107 18.3657 3.58738L17.76 7H21.1649C21.4755 7 21.711 7.28023 21.6574 7.58619L21.4824 8.58619C21.4406 8.82544 21.2328 9 20.9899 9H17.41L16.35 15H19.7549C20.0655 15 20.301 15.2802 20.2474 15.5862L20.0724 16.5862C20.0306 16.8254 19.8228 17 19.5799 17H16L15.3632 20.5874C15.3209 20.8261 15.1134 21 14.8709 21H13.8866C13.5755 21 13.3399 20.7189 13.3943 20.4126L14 17H8.00001L7.36325 20.5874C7.32088 20.8261 7.11337 21 6.87094 21H5.88657ZM9.41045 9L8.35045 15H14.3504L15.4104 9H9.41045Z"></path></svg>
             <h5 class="m-0" style={{color:'white'}}>general</h5>
@@ -67,7 +66,7 @@ function Chat() {
         </div>
         </div>
         </div>
-        <div class="row" style={{height:'90%'}}>
+        <div class="row" style={{height:'90%',borderTop:'2px solid black'}}>
             <div class="col-12 col-md-9 col-xl-9 p-0 m-0" style={{height:'100%'}}>
 
             <div class="d-flex flex-column justify-content-between" style={{height:'100%',overflowX:'visible'}}>
@@ -85,7 +84,7 @@ function Chat() {
            </div>
           
            <div class="d-flex justify-content-center align-items-start p-0 m-0" style={{height:'60px',width:'100%',backgroundColor:'rgb(54,57,63)',position:'relative'}}>
-            <div class="d-flex align-items-center justify-content-between" style={{width:'95%',backgroundColor:'rgb(64,68,75)',height:'80%',borderRadius:'8px'}}>
+            <div class="d-flex align-items-center justify-content-between" style={{width:'95%',backgroundColor:'rgb(64,68,75)',height:'80%',borderRadius:'8px',overflowX:'auto'}}>
             <div class="d-flex align-items-center">
             <svg width="24" height="24" viewBox="0 0 24 24" class="mx-3 ico-input"><path class="attachButtonPlus-jWVFah"  d="M12 2.00098C6.486 2.00098 2 6.48698 2 12.001C2 17.515 6.486 22.001 12 22.001C17.514 22.001 22 17.515 22 12.001C22 6.48698 17.514 2.00098 12 2.00098ZM17 13.001H13V17.001H11V13.001H7V11.001H11V7.00098H13V11.001H17V13.001Z"></path></svg>
                 <p style={{color:'rgba(255,255,255,0.2)'}} class="m-0">Message #general</p>
--- src/Main.js
+++ src/Main.js
@@ -7,18 +7,16 @@ function Main() {
     return (
         <>
 
-<div class="container-fluid p-0 no-gutters" style={{background:'blue',margin:0}}>
-        <div class="row p-0 m-0">
-            <div class="col-3 col-md-1 col-xl-1 p-0" style={{backgroundColor:'rgb(32,34,37)',height:'100vh'}}>
-                <SideServers style={{marginTop:'30px'}} />
+<div class="p-0 no-gutters" style={{height:'100vh',width:'100vw'}}>
+<div class="row p-0 m-0" style={{height:'100vh',width:'100vw',overflow:'hidden'}}>
+            <div class="col-3 col-md-1 col-xl-1 p-0 m-0" style={{backgroundColor:'rgb(32,34,37)',height:'100vh'}}>
+                <SideServers class="m-1" />
             </div>
             <div class="col-0 d-none d-xl-block col-md-3 col-xl-3 p-0 m-0" style={{backgroundColor:'rgb(47,49,54)',height:'100vh'}}>
                <Channels />
             </div>
             <div class="col-9  col-md-11 col-xl-8 p-0 m-0" style={{backgroundColor:'rgb(54,57,63)',height:'100vh',overflowX:'auto'}}>
-               <div class="p-0 m-0" style={{width:'100%'}}>
                <Chat />
-               </div>
             </div>
         </div>
 </div>
